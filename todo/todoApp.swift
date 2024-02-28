6865155ab1eec3d54e8dcab7e40229b92fa6d502
marcellinuselbert <elbertmarcellinus@gmail.com>
2024-02-28 12:31:17 +0700

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Marcellinus Elbert on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
